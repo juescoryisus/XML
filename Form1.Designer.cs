afcc7547a761b14cdde9325ce6183a8859deccf1
juescoryisus <jesuseduardog204@gmail.com>
2024-04-04 19:11:41 -0600

    primer repositorio

---
+++ Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace XML_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnCargar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(85, 46);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(139, 64);
+            this.btnGuardar.TabIndex = 0;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnCargar
+            // 
+            this.btnCargar.Location = new System.Drawing.Point(85, 116);
+            this.btnCargar.Name = "btnCargar";
+            this.btnCargar.Size = new System.Drawing.Size(139, 64);
+            this.btnCargar.TabIndex = 1;
+            this.btnCargar.Text = "Cargar";
+            this.btnCargar.UseVisualStyleBackColor = true;
+            this.btnCargar.Click += new System.EventHandler(this.btnCargar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(299, 250);
+            this.Controls.Add(this.btnCargar);
+            this.Controls.Add(this.btnGuardar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnCargar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace XML_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+
+            XmlElement root = xmlDoc.CreateElement("Personas");
+            xmlDoc.AppendChild(root);
+
+            XmlElement persona1 = xmlDoc.CreateElement("Persona");
+            persona1.SetAttribute("id", "1");
+            persona1.InnerText = "Alejandro";
+            root.AppendChild(persona1);
+
+            XmlElement persona2 = xmlDoc.CreateElement("Persona");
+            persona2.SetAttribute("id", "2");
+            persona2.InnerText = "Daylin";
+            root.AppendChild(persona2);
+
+            XmlElement persona3 = xmlDoc.CreateElement("Persona");
+            persona3.SetAttribute("id", "3");
+            persona3.InnerText = "Jesus";
+            root.AppendChild(persona3);
+
+            xmlDoc.Save("personas.xml");
+
+            MessageBox.Show("Archivo XML guardado correctamente.");
+        }
+
+        private void btnCargar_Click(object sender, EventArgs e)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+
+            try
+            {
+                xmlDoc.Load("personas.xml");
+
+                XmlNodeList personas = xmlDoc.GetElementsByTagName("Persona");
+
+                foreach (XmlNode persona in personas)
+                {
+                    MessageBox.Show($"ID: {persona.Attributes["id"].Value}, Nombre: {persona.InnerText}");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cargar el archivo XML: " + ex.Message);
+            }
+        }
+    }
+}
